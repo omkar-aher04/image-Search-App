a4baf6cad072835e473c954cbf045b01380f81cd
omkar-aher04 <aheromkar@gmail.com>
2025-06-02 02:21:41 +0530

    added a loader

--- script.js
+++ script.js
@@ -3,40 +3,48 @@ const searchBtn = document.querySelector(".btn");
 const form = document.querySelector("form");
 const resultDiv = document.querySelector(".resultdiv");
 const loadMore = document.querySelector(".loadMore");
+const loader = document.querySelector(".loader");
 const YOUR_ACCESS_KEY = "vBlpEnyq2bVxODWhZok3fPbx9sJfj1R4yAIUgzbZcuQ";
 
 let page = 1;
 let lastSearchTerm = "";
-
+loader.style.display = "none";
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1; // reset to first page
     resultDiv.innerHTML = ""; // clear previous results
     lastSearchTerm = query.value.trim();
     if (lastSearchTerm === "") return;
+    loader.style.display = "block";
     displaydata(lastSearchTerm);
+    
 });
 
 loadMore.addEventListener("click", () => {
+    loader.style.display = "block";
     displaydata(lastSearchTerm);
 });
 
 async function displaydata(searchTerm) {
     try {
+        loadMore.style.display = "none";
         const response = await fetch(`https://api.unsplash.com/search/photos?query=${searchTerm}&page=${page}&per_page=10`, {
             headers: {
                 Authorization: `Client-ID ${YOUR_ACCESS_KEY}`
             }
         });
-
         const result = await response.json();
+        
         showOnDisplay(result.results);
+        
         loadMore.style.display = "block"; 
         page++; 
         query.value = ""; 
 
     } catch (error) {
         console.log("Try again later", error);
+    } finally {
+        loader.style.display = "none";
     }
 }
 
@@ -57,5 +65,6 @@ function showOnDisplay(obj) {
         imgContainer.append(img, desc);
         fragment.append(imgContainer);
     });
+    
     resultDiv.append(fragment);
 }
